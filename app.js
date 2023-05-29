cd37e37240878d0e9bdc787df9205394744d086c
unknown <ege33tekin@outlook.com>
2023-05-29 12:36:34 +0300

    education

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 let line1 = document.querySelector('.line-about');
 let line2 = document.querySelector('.line-skills');
-let line3 = document.querySelector('.line-contact');
+let line3 = document.querySelector('.line-education');
+
 
 window.onscroll = () => { 
     let pos = window.scrollY;
+    console.log(pos);
     line1.style.right = `${pos - 150}px`
     line2.style.right = `${pos - 1150}px`
-    line3.style.right = `${pos - 2150}px`
+    line3.style.right = `${pos - 3050}px`
 }
