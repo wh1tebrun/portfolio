5596ee76b912d67d71640677aa48bcbaee792fd1
unknown <ege33tekin@outlook.com>
2023-05-29 18:31:59 +0300

    parts

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 let line1 = document.querySelector('.line-about');
 let line2 = document.querySelector('.line-skills');
 let line3 = document.querySelector('.line-education');
+let line4 = document.querySelector('.line-projects');
+let line5 = document.querySelector('.line-contact');
 
 
 window.onscroll = () => { 
@@ -9,4 +11,6 @@ window.onscroll = () => {
     line1.style.right = `${pos - 150}px`
     line2.style.right = `${pos - 1150}px`
     line3.style.right = `${pos - 3050}px`
+    line4.style.right = `${pos - 4250}px`
+    line5.style.right = `${pos - 5650}px`
 }
